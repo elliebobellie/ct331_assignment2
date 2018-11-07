e51c30a938faf8afe872f2d0df9b22d4654dca93
elliebobellie <23409382+elliebobellie@users.noreply.github.com>
2018-11-07 14:52:22 +0000

    e and fixed spacing issue

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -4,6 +4,8 @@
 (provide ins_end)
 (provide count_top_level)
 (provide count_instances)
+(provide count_instances_tr_helper)
+(provide count_instances_tr)
 
 ;(A)
 (define (ins_beg el lst)
@@ -24,18 +26,29 @@
 ;(C)
 (define (count_top_level list)
   (cond ((null? list) 0)
-        (else(+1 (count_top_level (cdr list))))
+        (else(+ 1 (count_top_level (cdr list))))
         )
  )
 
 ;(D)
 (define (count_instances at list)
   (cond ((null? list) 0)
-        ((= at (car list)) (+1 (count_instances at (cdr list))))
+        ((= at (car list)) (+ 1 (count_instances at (cdr list))))
         ((count_instances at (cdr list)))
    )
  )
 
 ;(E)
+(define (count_instances_tr_helper at list)
+  (count_instances_tr at list 0)
+ )
+
+(define (count_instances_tr at list total)
+  (cond ((null? list) total)
+        ((= at (car list)) (count_instances_tr at (cdr list) (+ total 1)))
+        (else (count_instances_tr at (cdr list) total) )
+        )
+  
+ )
 
 
